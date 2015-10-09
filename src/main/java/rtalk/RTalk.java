8b2bfb828c2d710d14dbcbd2f1f93dee69aa3a6b
Alex Zhukov <zhukov.alex@gmail.com>
2015-10-09 20:32:48 +0300

    getTube

--- src/main/java/rtalk/RTalk.java
+++ src/main/java/rtalk/RTalk.java
@@ -35,6 +35,10 @@ public class RTalk extends RedisDao {
     }
 
     private String tube = "default";
+    
+    public String getTube() {
+        return tube;
+    }
 
     /**
      * use <tube>\r\n
